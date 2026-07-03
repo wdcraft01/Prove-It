cd1f2bb5d52954086c1002bc45e101aa7bd45cbf
Warren Craft <wdcraft@unm.edu>
2026-07-03 13:58:42 -0600

    Add minimal working Union.prove_by_cases() method
    Add a minimal working Union.prove_by_cases() method, based on the non-conditioned portion of the analogous Set.prove_by_cases() method.

--- packages/proveit/logic/sets/unification/union.py
+++ packages/proveit/logic/sets/unification/union.py
@@ -34,4 +34,73 @@ class Union(Operation):
         _S = superset
         return union_inclusion.instantiate(
                     {A:_A, m:_m, S:_S})
+
+    # @prover
+    def prove_by_cases(self, forall_stmt, **defaults_config):
+        '''
+        UNDER CONSTRUCTION.
+        BASED ON SET.PROVE_BY_CASES().
+
+        For the Union S = A1 U A2 U ... U Am (i.e., self), and given
+        a universal quantification 'forall_stmt' over the set S of the
+        form [Forall_{x in S} P(x)], conclude and return the Forall
+        expression knowing/assuming:
+
+            Forall_{x in A1} P(x) AND Forall_{x in A2} P(x) AND ...
+                AND Forall_{x in Am} P(x)
+        '''
+        from proveit import P, ExprTuple, Function, var_range
+        from proveit.logic import Forall, InSet
+        from proveit.numbers import one
+        assert(isinstance(forall_stmt, Forall)), (
+            "May only call the prove_by_cases() method of the Union "
+            "class using a Forall (universally quantified) expression "
+            "as the first argument. Union.prove_by_cases() was called "
+            f"with arg 'forall_stmt' = {forall_stmt}.")
+        assert(forall_stmt.conditions.num_entries() >= 1), (
+            "When calling the prove_by_cases() method of the Union "
+            "class, the Forall argument should have (at least) "
+            "a domain condition consisting of a Union.")
+        assert(isinstance(forall_stmt.conditions[0], InSet)), (
+            "When calling the prove_by_cases() method of the Union "
+            "class, the domain condition for the Forall argument "
+            "should appear as the first element in the Forall.conditions. "
+            "Consider using the 'domain=' kwarg when specifying the "
+            "domain when constructing your Forall expression, or "
+            "specify the domain using an InSet expression as the first "
+            "of the conditions you specify.")
+
+        from . import true_in_each_then_true_in_union
+
+        if (forall_stmt.conditions.num_entries() > 1):
+            raise NotImplementedError(
+                "Union.prove_by_cases() implemented only for Forall "
+                "expressions without explicit conditions. The 'forall_stmt' "
+                f"argument was {forall_stmt}.")
+
+        # forall_{x in A1 U A2 U ... U Am} P(x), assuming/knowing P(x)
+        # for all x in A1, for all x in A2, ... for all x in Am.
+        # This is the basic case where the only condition in the
+        # forall_stmt argument is the domain specification.
+
+        # Number of sets [A1, A2, ..., Am] making up the Union
+        _m_sub = self.operands.num_elements()
+
+        # Union component elements to substitute
+        var_range_update = var_range(A, one, _m_sub)
+        _var_range_sub = self.operands
+
+        # Predicate re-definition (using user-supplied instance_var)
+        Px = Function(P, forall_stmt.instance_var)
+
+        # Predicate to substitute
+        _Px_sub = forall_stmt.instance_expr
+
+        # Instance var to substitute
+        _x_sub = forall_stmt.instance_var
+
+        return (true_in_each_then_true_in_union.instantiate(
+            {m: _m_sub, ExprTuple(var_range_update): _var_range_sub,
+             x: _x_sub, Px: _Px_sub}, num_forall_eliminations=2).
+            derive_consequent())
             
