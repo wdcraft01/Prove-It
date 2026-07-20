3527993497d0efbfe0565dc0c5d01bc04edc11c6
Warren Craft <wdcraft@unm.edu>
2026-07-20 17:19:29 -0600

    Begin outlining EmptySet.deduce_not_equal() method
    Begin outlining the EmptySet.deduce_not_equal() method, intended to be called form the NotEquals.conclude() method.

--- packages/proveit/logic/sets/empty_set.py
+++ packages/proveit/logic/sets/empty_set.py
@@ -1,5 +1,7 @@
-from proveit import Literal
+from proveit import x, A, Literal
+from proveit.logic import Exists
 from proveit.logic.irreducible_value import IrreducibleValue
+from proveit.logic.sets import InSet
 
 
 class EmptySetLiteral(Literal, IrreducibleValue):
@@ -7,3 +9,19 @@ class EmptySetLiteral(Literal, IrreducibleValue):
         Literal.__init__(
             self, string_format='emptyset', latex_format=r'\emptyset',
             styles=styles)
+
+    # def deduce_not_equal(self, other, **defaults_config):
+    #     '''
+    #     Deduce that self (i.e., the empty set) is not equal to other.
+    #     Currently we address two special cases:
+    #     (1) We can conclude that A ≠ EmptySet if we know there exists
+    #         some x in A.
+    #     (2) We can conclude that A U B ≠ EmptySet if we know that
+    #         either A ≠ EmptySet or B ≠ EmptySet (or both). This can
+    #         be generalized to the case of A1 U A2 U ... U An ≠ EmptySet.
+    #     '''
+
+    #     # (1) There exists x in A.
+    #     exists_x_in_A = Exists(x, InSet(x, A))
+    #     if exists_x_in_A.readily_provable():
+
